7e03cda52205dfb9f48b3b92541fed807dff317d
zagalapawel <zagalapawel@gmail.com>
2020-10-26 18:55:11 +0100

    input & output styling and configuration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-import './user_transactions.dart';
-
 class NewTransaction extends StatelessWidget {
   final Function newTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
   NewTransaction(this.newTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    newTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +33,7 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
               // onChanged: (value) {
               //   titleInput = value;
               // },
@@ -28,6 +41,8 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(), //*(it has to get an value "_")
               // onChanged: (value) {
               //   amountInput = value;
               // },
@@ -35,12 +50,7 @@ class NewTransaction extends StatelessWidget {
             FlatButton(
               child: Text('Add transaction'),
               textColor: Colors.purple,
-              onPressed: () {
-                newTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
             ),
           ],
         ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,9 +31,8 @@ class TransactionList extends StatelessWidget {
                   padding: EdgeInsets.all(10),
                   child: Text(
                     '\$' +
-                        transactions[index]
-                            .amount
-                            .toString(), //Can do the same by '\$${tx.amount}'
+                        transactions[index].amount.toStringAsFixed(
+                            2), //Can do the same by '\$${tx.amount}'
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
